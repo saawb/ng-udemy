229db2404e3fd70f9ef501362c4a2c9196930c1a
Shamil <shamil98@gmail.com>
2026-04-25 13:17:34 -0700

    added user component, self-closing tags, into app.html and app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { User } from "./user/user";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, User],
   templateUrl: './app.html',
-  styleUrl: './app.less'
+  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('ng-udemy');
