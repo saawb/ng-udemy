31dbb75ffc60f875f5138ba2e4950f1383a88881
Shamil <shamil98@gmail.com>
2026-04-25 16:51:09 -0700

    user components is shown as a list, @for conditional added to display all users

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+    {
+    id: 'u1',
+    name: 'Jordan Smith',
+    avatar: 'user-1.jpg',
+    }, 
+    {
+        id: 'u2',
+        name: 'Marcus Brown',
+        avatar: 'user-2.jpg',
+    },
+    {
+        id: 'u3',
+        name: 'Jeff Gold',
+        avatar: 'user-3.jpg',
+    },
+    {
+        id: 'u4',
+        name: 'David Miller',
+        avatar: 'user-4.jpg',
+    },
+    {
+        id: 'u5',
+        name: 'Priya Patel',
+        avatar: 'user-5.jpg',
+    },
+  {
+        id: 'u6',
+        name: 'Shamil Auwal',
+        avatar: 'user-6.jpg',
+  }
+]
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users'
+
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -6,4 +9,11 @@ import { Component } from '@angular/core';
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
-export class User {}
+export class User {
+  users = DUMMY_USERS;
+  imagePath = 'assets/users/';
+
+  onSelectUser () {
+    console.log('Clicked')
+  }
+}
